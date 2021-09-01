7b944b788f9c8fc0d874cc7ca794bde084f9f111
davidtmasin <davidteixeira.info@gmail.com>
2021-09-01 13:47:32 -0300

    Manipulando Strings - JAVA IFCE FIC EM CASA

---
+++ PlayingToStrings.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    String a = "Xablau";
+	    String b;
+	    
+	    Scanner leitor = new Scanner(System.in);
+	    
+	    System.out.println("Digite um nome");
+	    b = leitor.next();
+	    
+	    System.out.println(a.equals(b));
+	    System.out.println(a.length());
+	    System.out.println(a.charAt(2));
+	    System.out.println(a.toLowerCase());
+	    System.out.println(a.toUpperCase());
+	}
+}
